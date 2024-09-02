1b49913574c4f6340f25fa0b7bcb462b5bf78482
Jeff Hykin <jeff.hykin@gmail.com>
2024-09-01 19:55:00 -0500

    reorganize code

--- login_thingy/backend.js
+++ login_thingy/backend.js
@@ -1,31 +1,5 @@
-import ethUtil from 'https://esm.sh/ethereumjs-util@7.1.5'
-import { toHex } from './utils.js'
 import { generateKeys, encrypt, decrypt, sign, verify, keyStringToKeyObjects, extractDerPrivateKey, extractDerPublicKey } from "../source/encryption.js"
-
-const tool = {
-    createChallengeMessage() {
-        return `EzAuth trying to login at ${new Date()}\nverification: ${Math.random().toString(36).slice(2)}`
-    }
-    // TODO: I'd really like to remove ethUtil.
-    // it doesn't run in the browser cause it depends on the node.js Buffer class
-    // .ecrecover and .pubToAddress are the important functions. It would be good to find independent versions of them
-    signatureIsValid(originalMessage, signature, signerAddress) {
-        // Convert the digest and signature from hex strings to buffers
-        var digestBuffer = new Uint8Array(ethUtil.hashPersonalMessage(ethUtil.toBuffer(toHex(originalMessage))))
-        var signatureBuffer = new Uint8Array(ethUtil.toBuffer(signature))
-        
-        // Split the signature to get r, s, and v
-        var { v, r, s } = ethUtil.fromRpcSig(signatureBuffer)
-
-        // Recover the public key from the signature
-        var publicKey = ethUtil.ecrecover(digestBuffer, v, r, s)
-        // Convert the recovered public key to an address
-        var recoveredAddress = ethUtil.pubToAddress(publicKey).toString('hex')
-
-        // Compare the recovered address with the provided address
-        return recoveredAddress.toLowerCase() === ethUtil.stripHexPrefix(signerAddress).toLowerCase()
-    }
-}
+import { tool } from './auth_server.js'
 
 // 
 // example
