2366eaaf23bc98f072a81e450fc934237c36a602
Jeff Hykin <jeff.hykin@gmail.com>
2024-12-07 13:29:49 -0600

    code renders onChange

--- other/js_live/dev.js
+++ other/js_live/dev.js
@@ -1,134 +1,128 @@
 // 
 // args
 // 
-import { parseArgs, flag, required, initialValue } from "https://deno.land/x/good@1.13.3.0/flattened/parse_args.js"
-import { didYouMean } from "https://deno.land/x/good@1.13.3.0/flattened/did_you_mean.js"
-import $ from "https://deno.land/x/dax@0.39.2/mod.ts"
-const $$ = (...args)=>$(...args).noThrow()
+    import { parseArgs, flag, required, initialValue } from "https://deno.land/x/good@1.13.3.0/flattened/parse_args.js"
+    import { didYouMean } from "https://deno.land/x/good@1.13.3.0/flattened/did_you_mean.js"
+    import $ from "https://deno.land/x/dax@0.39.2/mod.ts"
+    const $$ = (...args)=>$(...args).noThrow()
 
-const argsInfo = parseArgs({
-    rawArgs: Deno.args,
-    fields: [
-        [["--debug", "-d", ], flag, ],
-        [[0, "--file"], required, (str)=>str],
-        [["--port"], initialValue(`8080`), (str)=>str],
-        [["--address"], initialValue(`localhost`), (str)=>str],
-    ],
-    namedArgsStopper: "--",
-    allowNameRepeats: true,
-    valueTransformer: JSON.parse,
-    isolateArgsAfterStopper: false,
-    argsByNameSatisfiesNumberedArg: true,
-    implicitNamePattern: /^(--|-)[a-zA-Z0-9\-_]+$/,
-    implictFlagPattern: null,
-})
-didYouMean({
-    givenWords: Object.keys(argsInfo.implicitArgsByName).filter(each=>each.startsWith(`-`)),
-    possibleWords: Object.keys(argsInfo.explicitArgsByName).filter(each=>each.startsWith(`-`)),
-    autoThrow: true,
-    suggestionLimit: 1,
-})
-const args = argsInfo.simplifiedNames
+    const argsInfo = parseArgs({
+        rawArgs: Deno.args,
+        fields: [
+            [["--debug", "-d", ], flag, ],
+            [[0, "--file"], required, (str)=>str],
+            [["--port"], initialValue(`8080`), (str)=>str],
+            [["--address"], initialValue(`localhost`), (str)=>str],
+        ],
+        namedArgsStopper: "--",
+        allowNameRepeats: true,
+        valueTransformer: JSON.parse,
+        isolateArgsAfterStopper: false,
+        argsByNameSatisfiesNumberedArg: true,
+        implicitNamePattern: /^(--|-)[a-zA-Z0-9\-_]+$/,
+        implictFlagPattern: null,
+    })
+    didYouMean({
+        givenWords: Object.keys(argsInfo.implicitArgsByName).filter(each=>each.startsWith(`-`)),
+        possibleWords: Object.keys(argsInfo.explicitArgsByName).filter(each=>each.startsWith(`-`)),
+        autoThrow: true,
+        suggestionLimit: 1,
+    })
+    const args = argsInfo.simplifiedNames
 
 // 
 // parsing JS
 // 
-import { FileSystem, glob } from "https://deno.land/x/quickr@0.6.72/main/file_system.js"
-import { run, hasCommand, throwIfFails, zipInto, mergeInto, returnAsString, Timeout, Env, Cwd, Stdin, Stdout, Stderr, Out, Overwrite, AppendTo, } from "https://deno.land/x/quickr@0.6.72/main/run.js"
-import { Console, clearAnsiStylesFrom, black, white, red, green, blue, yellow, cyan, magenta, lightBlack, lightWhite, lightRed, lightGreen, lightBlue, lightYellow, lightMagenta, lightCyan, blackBackground, whiteBackground, redBackground, greenBackground, blueBackground, yellowBackground, magentaBackground, cyanBackground, lightBlackBackground, lightRedBackground, lightGreenBackground, lightYellowBackground, lightBlueBackground, lightMagentaBackground, lightCyanBackground, lightWhiteBackground, bold, reset, dim, italic, underline, inverse, strikethrough, gray, grey, lightGray, lightGrey, grayBackground, greyBackground, lightGrayBackground, lightGreyBackground, } from "https://deno.land/x/quickr@0.6.72/main/console.js"
+    import { FileSystem, glob } from "https://deno.land/x/quickr@0.6.72/main/file_system.js"
+    import { run, hasCommand, throwIfFails, zipInto, mergeInto, returnAsString, Timeout, Env, Cwd, Stdin, Stdout, Stderr, Out, Overwrite, AppendTo, } from "https://deno.land/x/quickr@0.6.72/main/run.js"
+    import { Console, clearAnsiStylesFrom, black, white, red, green, blue, yellow, cyan, magenta, lightBlack, lightWhite, lightRed, lightGreen, lightBlue, lightYellow, lightMagenta, lightCyan, blackBackground, whiteBackground, redBackground, greenBackground, blueBackground, yellowBackground, magentaBackground, cyanBackground, lightBlackBackground, lightRedBackground, lightGreenBackground, lightYellowBackground, lightBlueBackground, lightMagentaBackground, lightCyanBackground, lightWhiteBackground, bold, reset, dim, italic, underline, inverse, strikethrough, gray, grey, lightGray, lightGrey, grayBackground, greyBackground, lightGrayBackground, lightGreyBackground, } from "https://deno.land/x/quickr@0.6.72/main/console.js"
 
-import { generateNewJs } from "./parsing.js"
-const websocketAddress = `ws://${args.address}:${args.port}`
-let latestCode = generateNewJs(await FileSystem.read(args.file), websocketAddress)
+    import { generateNewJs } from "./parsing.js"
+    const websocketAddress = `ws://${args.address}:${args.port}`
+    let latestCode = await FileSystem.read(args.file)
+    let generatedCode = generateNewJs(latestCode, websocketAddress)
 
 // 
 // socket setup
 // 
-let allSockets = []
-const broadcast = (data)=>{
-    for (const each of allSockets) {
-        each.send(data)
-    }
-}
-const onReceive = (event, socket) => {
-    console.debug(`event is:`,event)
-}
-Deno.serve(
-    { port: args.port, hostname: args.address },
-    async (req) => {
-
-        // 
-        // normal server response
-        // 
-        if (req.headers.get("upgrade") != "websocket") {
-            const url = new URL(req.url)
-            const path = url.pathname
-            
-            // server yaml.js library
-            if (path.match(/\/?yaml.js$/)) {
-                return new Response(await FileSystem.read(`${FileSystem.thisFolder}/yaml.js`), { status: 200, headers: { "Content-Type": "text/javascript" } })
-            }
-            
-            // follow up with latestCode
-            setTimeout(() => {
-                broadcast(yaml.stringify({latestCode}))
-            }, 0)
-            // index.html
-            return new Response(
-                `<!DOCTYPE html>
-                    <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <title>Document</title>
-                    </head>
-                    <body>
-                    </body>
-                    <script type="module">
-                        import * as yaml from "./yaml.js"
-                        let socket = new WebSocket("${websocketAddress}")
-                        window.socket = socket
-                        socket.addEventListener("open",()=>{
-                            console.debug("connected")
-                            socket.send("Hello World!")
-                        })
-                        socket.addEventListener("message",(message,...args)=>{
-                            const data = yaml.parse(message.data)
-                            console.debug(\`data is:\`,data)
-                        })
-                        ${await FileSystem.read(`${FileSystem.thisFolder}/web.js`)}
-                    </script>
-                </html>`,
-                { status: 200, headers: { "Content-Type": "text/html" } }
-            )
-            // return new Response(null, { status: 501 })
+    let allSockets = []
+    const broadcast = (data)=>{
+        for (const each of allSockets) {
+            each.send(data)
         }
-
-        // 
-        // socket
-        // 
-
-        const { socket, response } = Deno.upgradeWebSocket(req)
-        socket.addEventListener("open",()=>allSockets.push(socket))
-        socket.addEventListener("message", (event)=>onReceive(event, socket))
-        socket.addEventListener("close",()=>allSockets.splice(allSockets.indexOf(socket),1))
-
-        return response
     }
-)
+    const onReceive = (event, socket) => {
+        console.debug(`event is:`,event)
+    }
+    Deno.serve(
+        { port: args.port, hostname: args.address },
+        async (req) => {
+
+            // 
+            // normal server response
+            // 
+            if (req.headers.get("upgrade") != "websocket") {
+                const url = new URL(req.url)
+                const path = url.pathname
+                
+                // server yaml.js library
+                if (path.match(/\/?yaml.js$/)) {
+                    return new Response(await FileSystem.read(`${FileSystem.thisFolder}/yaml.js`), { status: 200, headers: { "Content-Type": "text/javascript" } })
+                }
+                
+                // follow up with latestCode
+                setTimeout(() => {
+                    broadcast(yaml.stringify({latestCode}))
+                }, 0)
+                // index.html
+                return new Response(
+                    `<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>Document</title>
+                        </head>
+                        <body>
+                        </body>
+                        <script type="module">
+                            import * as yaml from "./yaml.js"
+                            let socket = new WebSocket("${websocketAddress}")
+                            window.socket = socket
+                            ${await FileSystem.read(`${FileSystem.thisFolder}/web.js`)}
+                        </script>
+                    </html>`,
+                    { status: 200, headers: { "Content-Type": "text/html" } }
+                )
+                // return new Response(null, { status: 501 })
+            }
+
+            // 
+            // socket
+            // 
+
+            const { socket, response } = Deno.upgradeWebSocket(req)
+            socket.addEventListener("open",()=>allSockets.push(socket))
+            socket.addEventListener("message", (event)=>onReceive(event, socket))
+            socket.addEventListener("close",()=>allSockets.splice(allSockets.indexOf(socket),1))
+
+            return response
+        }
+    )
 
 // 
 // watch loop
 // 
-import * as yaml from "https://deno.land/std@0.168.0/encoding/yaml.ts"
-let watcher = Deno.watchFs(args.file)
-for await (const event of watcher) {
-    latestCode = generateNewJs(await FileSystem.read(args.file), websocketAddress)
-    broadcast(yaml.stringify({latestCode}))
-    FileSystem.write({
-        path: args.file+".out.js",
-        data: latestCode,
-    }).then(()=>{
-        $$`deno run -A ${args.file+".out.js"}`
-    })
-}
+    import * as yaml from "https://deno.land/std@0.168.0/encoding/yaml.ts"
+    let watcher = Deno.watchFs(args.file)
+    for await (const event of watcher) {
+        latestCode = await FileSystem.read(args.file)
+        const generatedCode = generateNewJs(await FileSystem.read(args.file), websocketAddress)
+        broadcast(yaml.stringify({latestCode}))
+        FileSystem.write({
+            path: args.file+".out.js",
+            data: latestCode,
+        }).then(()=>{
+            $$`deno run -A ${args.file+".out.js"}`
+        })
+    }
--- other/js_live/web.js
+++ other/js_live/web.js
@@ -9,18 +9,122 @@ import { zip, enumerate, count, permute, combinations, wrapAroundGet } from "htt
 
 import storageObject from "https://esm.sh/gh/jeff-hykin/storage-object@0.0.2.0/main.js"
 
+import CM from 'https://esm.sh/gh/jeff-hykin/codemirror_esm@0.0.2.0/main.js'
+import atomOne from 'https://esm.sh/gh/jeff-hykin/codemirror_esm@0.0.2.0/themes/atomone.js'
+
+const { basicSetup } = CM["codemirror"]
+const { EditorView, keymap } = CM["@codemirror/view"]
+const { EditorState, Prec } = CM["@codemirror/state"]
+const { javascript } = CM["@codemirror/lang-javascript"]
+const { tags: t } = CM['@lezer/highlight']
+const { themeToExtension } = CM["@jeff-hykin/theme-tools"]
+
 const { html } = Elemental({
     ...components,
+    Editor,
 })
 
+let lineHeight = `1.5em`
+let editorElement
 document.body = html`
-    <body font-size=15px background-color=whitesmoke overflow=scroll width=100vw>
-        <Column>
-            <span>Howdy!</span>
-            <span>Howdy!</span>
-            <span>Howdy!</span>
-        </Column>
+    <body font-size=15px background-color=whitesmoke overflow=scroll width=100vw padding=0 margin=0>
+        <Row>
+            ${editorElement=html`
+                <Editor initialText="console.log('howdy')\n\n\n\n" width=50vw />
+            `}
+        </Row>
     </body>
 `
 
-showToast("Hello World!")
+socket.addEventListener("open",()=>{
+    console.debug("connected")
+})
+
+socket.addEventListener("message",(message,...args)=>{
+    const data = yaml.parse(message.data)
+    if (data.latestCode) {
+        // inefficient, but it works 
+        editorElement.replaceWith(html`<Editor initialText="${data.latestCode}" width=50vw />`)
+        // editorElement.code = data.latestCode
+    }
+})
+
+function Editor({initialText, onChange, onRun, ...props}={}) {
+    const parent = document.createElement("div")
+    let editor = new EditorView({
+        parent,
+        state: EditorState.create({
+            doc: initialText,
+            extensions: [
+                basicSetup, // ctrl+z = undo, and other stuff like that
+                javascript(), // highlighting hooks
+                
+                // 
+                // theme choice
+                // 
+                themeToExtension(
+                    atomOne({
+                        variant:"dark", // "light" | "dark"
+                        settings:{
+                            // can override defaults:
+                            // background: 'hsl(286, 60%, 67%)',
+                            // foreground: 'hsl(286, 60%, 67%)',
+                            // caret: '#e06c75',
+                            // selection: '#e06c75',
+                            // lineHighlight: '#e06c75',
+                            // gutterBackground: '#e06c75',
+                            // gutterForeground: '#e06c75',
+                        },
+                        styles:[], // styles to add to the theme
+                        mutateThemeStyles:(style)=>style, // return null to remove a style entry from the theme
+                    })
+                ),
+                
+                // //
+                // // example: define your own keybinding
+                // //
+                // keymap.of([{
+                //     key: "Ctrl-Enter",
+                //     run: () =>{
+                //         console.log("User pressed Ctrl+Enter!")
+                //         return true
+                //     }}
+                // ]),
+                
+                // // 
+                // // example: overriding a default keybinding (from the basicSetup or another extension)
+                // // 
+                // // Prec = "precedence"
+                // Prec.high(keymap.of([{
+                //     key: "Ctrl-;",
+                //     run: () =>{
+                //         console.log("User pressed Ctrl+;!")
+                //         return true
+                //     }}
+                // ])),
+                
+                // // 
+                // // example: onChange
+                // // 
+                // EditorView.updateListener.of((update)=>{
+                //     // const codeString = editor.state.doc.text.join("\n")
+                //     // console.log("Something (no matter how small) happened!")
+                //     // console.log("I'm called on init, and twice per keypress")
+                //     // console.log("DON'T USE THIS UNLESS YOU HAVE TO!")
+                // })
+            ],
+        }),
+    })
+    const element = parent.children[0]
+    passAlongProps(element, props)
+    element.editor = editor
+    Object.defineProperties(element, {
+        code: { get() { return editor.state.doc.text.join("\n") } },
+    })
+    window.editor = editor
+    console.debug(`props is:`,props)
+    if (props.width) {
+        parent.children[0].style.width = props.width
+    }
+    return element
+}
