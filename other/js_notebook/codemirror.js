e529360e6d7449972f5202ab64aeab3a4243196a
Jeff Hykin <jeff.hykin@gmail.com>
2025-01-14 11:56:18 -0600

    delete js notebook

--- other/js_notebook/codemirror.js
+++
@@ -1,7 +0,0 @@
-// NOTE! this was only used to make a bundle
-import * as Main from "https://esm.sh/codemirror@6.0.1?target=es2020&dev"
-import * as View from "https://esm.sh/v135/@codemirror/view@6.35.0/es2022/view.mjs?target=es2020&dev"
-import * as State from "https://esm.sh/v135/@codemirror/state@6.4.1/es2022/state.mjs?target=es2020&dev"
-import { javascript } from "https://esm.sh/@codemirror/lang-javascript@6.0.1?target=es2020&dev"
-
-export { Main, View, State, javascript }
